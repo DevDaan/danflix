7ee56e669f046703f6da7d060255bf37ac1469de
Daniel Vieira <contatodanielnvieira@outlook.com>
2020-07-30 23:16:30 -0300

    Criando o nosso primeiro componente - Menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//SEMPRE COLOCAR O COMPONENTE COM A PRIMEIRA LETRA MAIUSCULA
+function Menu (){
+  return (
+    <header>
+      DanFlix
+    </header>
+  )
+};
+
+
+export default Menu;
